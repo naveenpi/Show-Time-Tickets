ed97f3d23906cf21bdc9b21fd0941d57e6013d53
naveenpi <gsnb.griet@gmail.com>
2020-06-23 16:41:03 -0500

    added reel icon into the image view

--- Codes/Android/app/src/main/java/com/example/showtime/MainActivity.java
+++ Codes/Android/app/src/main/java/com/example/showtime/MainActivity.java
@@ -3,12 +3,27 @@ package com.example.showtime;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    /**
+     * Button for login
+     */
+    Button login;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
+
+
 }
