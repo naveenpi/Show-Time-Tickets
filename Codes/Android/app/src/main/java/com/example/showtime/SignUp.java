d499b6d92181514d65b62e8f17f0fc3d1729efc3
Pranay Allikanti <pranaykumar0987@gmail.com>
2020-06-23 00:26:57 -0500

    Added EditText type in java

--- Codes/Android/app/src/main/java/com/example/showtime/SignUp.java
+++ Codes/Android/app/src/main/java/com/example/showtime/SignUp.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 
 public class SignUp extends AppCompatActivity {
 
+    EditText name,email,password,confirmpassword;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
