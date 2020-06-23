6c9b5d87b6dadf3cec5418b59c507949be164f7b
pavanreddy4227 <54386960+pavanreddy4227@users.noreply.github.com>
2020-06-23 18:51:26 -0500

    created the HomeActivity.java

--- Codes/Android/app/src/main/java/com/example/showtime/HomeActivity.java
+++ Codes/Android/app/src/main/java/com/example/showtime/HomeActivity.java
@@ -2,13 +2,19 @@ package com.example.showtime;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.PopupMenu;
 
-public class HomeActivity extends AppCompatActivity {
+public class HomeActivity extends AppCompatActivity
+{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
     }
+
+
 }
