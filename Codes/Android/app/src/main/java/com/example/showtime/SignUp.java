0787688b6bae18287dcf7d73ff3f984773cb297f
pranay0987 <pranaykumar0987@gmail.com>
2020-06-24 19:04:37 -0500

    Initialized the components in the User interface in SignUp.java

--- Codes/Android/app/src/main/java/com/example/showtime/SignUp.java
+++ Codes/Android/app/src/main/java/com/example/showtime/SignUp.java
@@ -7,16 +7,24 @@ import android.widget.EditText;
 
 public class SignUp extends AppCompatActivity {
 
-    EditText name,email,password,confirmpassword;
+    EditText name,email,password,confirmpassword, age, phone;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
-        
+        name=findViewById(R.id.FullName);
+        email=findViewById(R.id.Email_ID);
+        password=findViewById(R.id.password);
+        phone=findViewById(R.id.phone);
+        age=findViewById(R.id.age);
+        confirmpassword=findViewById(R.id.confirm_password);
         String Name;
         String EmailID;
+        String age;
+        String phone;
         String Password;
         String ConfirmPassword;
+       
         
     }
 }
