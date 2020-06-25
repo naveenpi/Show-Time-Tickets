ca44585976b76cdb4fe74872244329b0cf9f9ded
pavanreddy4227 <54386960+pavanreddy4227@users.noreply.github.com>
2020-06-24 19:53:06 -0500

    created a menu method for pop menu in HomeActivity.java

--- Codes/Android/app/src/main/java/com/example/showtime/HomeActivity.java
+++ Codes/Android/app/src/main/java/com/example/showtime/HomeActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.PopupMenu;
 
 public class HomeActivity extends AppCompatActivity implements PopupMenu.OnMenuItemClickListener
@@ -16,9 +17,14 @@ public class HomeActivity extends AppCompatActivity implements PopupMenu.OnMenuI
         setContentView(R.layout.activity_home);
     }
 
-
+    public void menu(View v) {
+        PopupMenu popup= new PopupMenu(this,v);
+        popup.setOnMenuItemClickListener(this);
+        popup.inflate(R.menu.navigation_menu);
+        popup.show();
+    }
     @Override
     public boolean onMenuItemClick(MenuItem item) {
-        return false;
+   
     }
 }
