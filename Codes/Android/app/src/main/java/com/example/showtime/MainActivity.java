ead603c2eca390bbdf4903ae1f79d024ff63c221
naveenpi <gsnb.griet@gmail.com>
2020-06-27 14:29:14 -0500

    Update the layout of login activity in activity_main.xml

--- Codes/Android/app/src/main/java/com/example/showtime/MainActivity.java
+++ Codes/Android/app/src/main/java/com/example/showtime/MainActivity.java
@@ -56,16 +56,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        public Dialog onCreateDialog(Bundle savedInstanceState) {
-            AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
-            builder.setTitle(R.string.action_sign_in)
-                    .setItems(R.array.optionsArray, new DialogInterface.OnClickListener() {
-                        public void onClick(DialogInterface dialog, int which) {
 
-                        }
-                    });
-            return builder.create();
-        }
         forgotPassword.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
