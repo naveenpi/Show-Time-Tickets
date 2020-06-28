7ff03e7db93a966bd454bb9162934da3accccd5f
praneeth323 <46974375+praneeth323@users.noreply.github.com>
2020-06-28 18:22:29 -0500

    Declare and initialized the components in ForgotPassword.java

--- Codes/Android/app/src/main/java/com/example/showtime/ForgotPassword.java
+++ Codes/Android/app/src/main/java/com/example/showtime/ForgotPassword.java
@@ -3,12 +3,27 @@ package com.example.showtime;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class ForgotPassword extends AppCompatActivity {
+
+    TextView titleForgotPassword,toLogin;
+    EditText emailID_ForgotPassword_EditText;
+    Button resetButton;
+    FirebaseAuth mAuth = FirebaseAuth.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forgot_password);
+
+        titleForgotPassword=findViewById(R.id.textView_Forgot_Password);
+        emailID_ForgotPassword_EditText=findViewById(R.id.Email_ID_Forgot_Password);
+        resetButton=findViewById(R.id.Reset_Forgot_Password);
+        toLogin=findViewById(R.id.toLogin);
     }
 }
