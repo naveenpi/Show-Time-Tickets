1bfccff1311fc4491b51091a0b171e73598a4487
prasadbobbilla <54373689+prasadbobbilla@users.noreply.github.com>
2020-06-28 18:02:11 -0500

    completed the logic for the register button in SignUp.java

--- Codes/Android/app/src/main/java/com/example/showtime/SignUp.java
+++ Codes/Android/app/src/main/java/com/example/showtime/SignUp.java
@@ -1,16 +1,28 @@
 package com.example.showtime;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -53,34 +65,44 @@ public class SignUp extends AppCompatActivity {
                 phoneRegisterString=phone_EditText.getText().toString();
                 ageString=age_EditText.getText().toString();
 
-                if(validations()) {
-                }}
-        });
-        if(name.getText().toString().isEmpty()){
-            name.setError("Enter the Name");
-        }
-        if(email.getText().toString().isEmpty()){
-            email.setError("Enter the Email");
-        }
-        if(password.getText().toString().isEmpty()){
-            password.setError("Enter the password");
-        }
-        if(confirmpassword.getText().toString().isEmpty()){
-            confirmpassword.setError("Enter the password again");
-        }
-        if(phone.getText().toString().isEmpty()){
-            phone.setError("Enter the phone number");
-        }
-        if(age.getText().toString().isEmpty()){
-            age.setError("Enter the age");
-        }
+                if(validations()){
+                    mAuth.createUserWithEmailAndPassword(emailRegisterString,passwordRegisterString).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if(task.isSuccessful()){
+                                customerID=mAuth.getCurrentUser().getUid();
+                                DocumentReference documentReference= db.collection("customers").document(customerID);
+                                Map<String,Object> customer= new HashMap<>();
+                                customer.put("FullName",fullNameString);
+                                customer.put("EmailID",emailRegisterString);
+                                customer.put("PhoneNumber",phoneRegisterString);
+                                customer.put("Age",ageString);
+                                documentReference.set(customer).addOnSuccessListener(new OnSuccessListener<Void>() {
+                                    @Override
+                                    public void onSuccess(Void aVoid) {
 
-        String Name;
-        String EmailID;
-        String age;
-        String phone;
-        String Password;
-        String ConfirmPassword;
+                                        Log.d("Successful registration", "user Profile is created for "+ customerID);
+                                    }
+
+                                }).addOnFailureListener(new OnFailureListener() {
+                                    @Override
+                                    public void onFailure(@NonNull Exception e) {
+                                        Log.d("Failure registration", "user Profile is not created"+ customerID);
+                                    }
+                                });
+                                startActivity(new Intent(getApplicationContext(),MainActivity.class));
+                                finish();
+                            }
+                            else{
+                                Toast.makeText(SignUp.this, "An error has occurred."+task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+                }
+
+            }
+        });
+
 
         
     }
