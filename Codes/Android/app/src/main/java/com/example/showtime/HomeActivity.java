bfd49c09898269209bc1a4a9fff38d85bfc0ae97
naveenpi <gsnb.griet@gmail.com>
2020-06-30 21:36:44 -0500

    completed the movie details function in the home activity

--- Codes/Android/app/src/main/java/com/example/showtime/HomeActivity.java
+++ Codes/Android/app/src/main/java/com/example/showtime/HomeActivity.java
@@ -4,19 +4,81 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.telecom.Call;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.PopupMenu;
+import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+
+import javax.annotation.Nullable;
 
 public class HomeActivity extends AppCompatActivity implements PopupMenu.OnMenuItemClickListener
 {
+
+    TextView b1,b2,b3,b4;
+    FirebaseAuth mAuth = FirebaseAuth.getInstance();
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+
+        b1=findViewById(R.id.textView);
+        b2=findViewById(R.id.textView2);
+        b3=findViewById(R.id.textView3);
+        b4=findViewById(R.id.textView4);
+
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent toDetails=new Intent(HomeActivity.this, MovieDetails.class);
+                toDetails.putExtra("movieId","bahubali2");
+                startActivity(toDetails);
+            }
+        });
+
+        b2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent toDetails=new Intent(HomeActivity.this, MovieDetails.class);
+                toDetails.putExtra("movieId","avengers");
+                startActivity(toDetails);
+
+            }
+        });
+
+        b3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent toDetails=new Intent(HomeActivity.this, MovieDetails.class);
+                toDetails.putExtra("movieId","thor");
+                startActivity(toDetails);
+
+            }
+        });
+
+        b4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent toDetails=new Intent(HomeActivity.this, MovieDetails.class);
+                toDetails.putExtra("movieId","ironman");
+                startActivity(toDetails);
+            }
+        });
     }
 
     public void menu(View v) {
---
+++ Codes/Android/app/src/main/java/com/example/showtime/MovieDetails.java
@@ -0,0 +1,62 @@
+package com.example.showtime;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+
+import javax.annotation.Nullable;
+
+public class MovieDetails extends AppCompatActivity {
+
+    TextView movieNameTextView,genreTextView, ratingTextView, priceTextView;
+    FirebaseAuth mAuth;
+    FirebaseFirestore db;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_movie_details);
+        movieNameTextView=findViewById(R.id.textView9);
+        genreTextView=findViewById(R.id.textView10);
+        ratingTextView=findViewById(R.id.textView11);
+        priceTextView=findViewById(R.id.textView12);
+
+        mAuth = FirebaseAuth.getInstance();
+        db = FirebaseFirestore.getInstance();
+
+        String movieId=getIntent().getStringExtra("movieId");
+        DocumentReference documentReference=db.collection("movie").document(movieId);
+        documentReference.addSnapshotListener(this, new EventListener<DocumentSnapshot>() {
+
+
+            @Override
+            public void onEvent(@Nullable DocumentSnapshot documentSnapshot, @Nullable FirebaseFirestoreException e) {
+
+                if (e != null) {
+                    Log.w("fail", "Listen failed.", e);
+                    return;
+                }
+
+                if(documentSnapshot!=null && documentSnapshot.exists()) {
+                    movieNameTextView.setText("Movie Name: "+documentSnapshot.getString("Movie Name"));
+                    genreTextView.setText("Genre: "+documentSnapshot.getString("Genre"));
+                    ratingTextView.setText("Rating: "+documentSnapshot.getString("Rating"));
+                    priceTextView.setText("Ticket Price: "+documentSnapshot.getString("Ticket Price"));
+                    Log.d("exists", "onEvent: Document do not exists");
+                }
+                else{
+                    Log.d("not", "onEvent: Document do not exists");
+                }
+            }
+        });
+
+    }
+}
