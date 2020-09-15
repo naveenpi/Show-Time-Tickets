e08874fbd9d13548a6c06bce84c9dbd35b860ebc
praneeth323 <46974375+praneeth323@users.noreply.github.com>
2020-09-15 00:49:22 -0500

    updating the payments to the firebase cloud firestore into the collection "payments"

--- Codes/Android/app/src/main/java/com/example/showtime/PaymentActivity.java
+++ Codes/Android/app/src/main/java/com/example/showtime/PaymentActivity.java
@@ -1,17 +1,27 @@
 package com.example.showtime;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 
-public class PaymentActivity extends AppCompatActivity {
+import java.util.HashMap;
+import java.util.Map;
+
+public class PaymentActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
     EditText cardNumberET, nameOnCardET;
     FirebaseAuth mAuth = FirebaseAuth.getInstance();
@@ -40,5 +50,55 @@ public class PaymentActivity extends AppCompatActivity {
         ArrayAdapter yearAdapter = new ArrayAdapter(this, android.R.layout.simple_spinner_dropdown_item, yearArray);
         yearSp.setAdapter(yearAdapter);
         yearSp.setOnItemSelectedListener(this);
+
+        addPayment.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+                Map<String,Object> payments= new HashMap<>();
+                payments.put("user",mAuth.getCurrentUser().getUid());
+                payments.put("cardNumber",cardNumberET.getText().toString());
+                payments.put("nameOnCard",nameOnCardET.getText().toString());
+                payments.put("month",month);
+                payments.put("year",year);
+
+                db.collection("payments").add(payments)
+                        .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                            @Override
+                            public void onSuccess(DocumentReference documentReference) {
+                                Log.d("Yes card", " Card is added ");
+                            }
+                        })
+                        .addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Log.d("No card", " Card is not added");
+                            }
+                        });
+
+
+            }
+        });
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+        Spinner monthSpin = (Spinner) parent;
+        Spinner yearSpin = (Spinner) parent;
+
+        if(monthSpin.getId() == R.id.SpinnerMonth){
+            month = monthArray[position];
+        }
+        if(yearSpin.getId() == R.id.SpinnerYear){
+            year = yearArray[position];
+        }
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
     }
 }
