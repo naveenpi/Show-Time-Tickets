9a399189fe184b83ac6f8ced25c2829c476e51b8
pavanreddy4227 <54386960+pavanreddy4227@users.noreply.github.com>
2020-09-15 01:20:32 -0500

    integrating with the database to get the booking collection values

--- Codes/Android/app/src/main/java/com/example/showtime/OrderCart.java
+++ Codes/Android/app/src/main/java/com/example/showtime/OrderCart.java
@@ -1,14 +1,72 @@
 package com.example.showtime;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.ItemTouchHelper;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import com.firebase.ui.firestore.FirestoreRecyclerOptions;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
 
 public class OrderCart extends AppCompatActivity {
+
+    RecyclerView recyclerView;
+    OrderAdapter adapter;
+    FirebaseFirestore firebaseFirestore = FirebaseFirestore.getInstance();
+    FirebaseAuth mAuth = FirebaseAuth.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_order_cart);
+        recyclerView = findViewById(R.id.recyclerViewOrderList);
+        constructRecyclerView();
+    }
+
+    private void constructRecyclerView() {
+
+        Query query = firebaseFirestore.collection("booking").whereEqualTo("user",mAuth.getCurrentUser().getUid());
+        Log.d("booking query",query.toString());
+        FirestoreRecyclerOptions<OrderModel> response = new FirestoreRecyclerOptions.Builder<OrderModel>()
+                .setQuery(query, OrderModel.class)
+                .build();
+
+        adapter = new OrderAdapter(response);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(adapter);
+
+        new ItemTouchHelper(new ItemTouchHelper.SimpleCallback(0,ItemTouchHelper.LEFT |ItemTouchHelper.RIGHT) {
+
+            @Override
+            public boolean onMove(@NonNull RecyclerView recyclerView, @NonNull RecyclerView.ViewHolder viewHolder, @NonNull RecyclerView.ViewHolder target) {
+                return false;
+            }
+
+            @Override
+            public void onSwiped(@NonNull RecyclerView.ViewHolder viewHolder, int direction) {
+                adapter.deleteItem(viewHolder.getAdapterPosition());
+            }
+
+        }).attachToRecyclerView(recyclerView);
+    }
+
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        adapter.stopListening();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        adapter.startListening();
     }
 }
