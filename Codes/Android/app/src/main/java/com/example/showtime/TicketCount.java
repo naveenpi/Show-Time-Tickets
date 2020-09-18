78c35c379509a2faedbce0e8c85b981df58c59ee
naveenpi <gsnb.griet@gmail.com>
2020-09-18 11:13:06 -0500

    received the intents in the TicketCount.java

--- Codes/Android/app/src/main/java/com/example/showtime/TicketCount.java
+++ Codes/Android/app/src/main/java/com/example/showtime/TicketCount.java
@@ -2,13 +2,48 @@ package com.example.showtime;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 public class TicketCount extends AppCompatActivity {
 
+    Button selectedSeats;
+    RadioGroup radioGroup;
+    Intent i;
+    int count=1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ticket_count);
+
+        selectedSeats = findViewById(R.id.selected_seats);
+        radioGroup = findViewById(R.id.seats);
+        i = getIntent();
+        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                RadioButton radioButton =findViewById(checkedId);
+                count =Integer.parseInt(radioButton.getText().toString());
+
+            }
+        });
+
+        selectedSeats.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent toSeating = new Intent(getApplicationContext(), SeatingActivity.class);
+                toSeating.putExtra("movieName",i.getStringExtra("movieName"));
+                toSeating.putExtra("date",i.getStringExtra("date"));
+                toSeating.putExtra("time",i.getStringExtra("time"));
+                toSeating.putExtra("count",count);
+                Log.d("seats count:",count+"");
+                startActivity(toSeating);
+            }
+        });
     }
 }
