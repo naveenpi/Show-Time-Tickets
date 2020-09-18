7873f4e06831f2c573602226ea1c22450dd0f8c3
chaitanyapopuri <popurichaitanya123@gmail.com>
2020-09-18 11:51:20 -0500

    Added the code for PaymentConsole.java for directing to add/ delete options

--- Codes/Android/app/src/main/java/com/example/showtime/PaymentConsole.java
+++ Codes/Android/app/src/main/java/com/example/showtime/PaymentConsole.java
@@ -2,13 +2,42 @@ package com.example.showtime;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class PaymentConsole extends AppCompatActivity {
 
+    Button add_button, delete_button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_payment_console);
+
+        add_button=findViewById(R.id.addButtonPayment);
+        delete_button=findViewById(R.id.deleteButtonPayment);
+
+        add_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(PaymentConsole.this, PaymentActivity.class));
+            }
+        });
+
+        delete_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(),DeletePayments.class));
+            }
+        });
+    }
+
+    public void logout(View view) {
+        FirebaseAuth.getInstance().signOut();
+        startActivity(new Intent(getApplicationContext(),MainActivity.class));
+        finish();
     }
 }
