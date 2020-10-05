9a19a965bedbf4684d359ad514d999964f03c0c3
pavanreddy4227 <54386960+pavanreddy4227@users.noreply.github.com>
2020-10-04 20:36:56 -0500

    Added functionality to connect to firebase in live chat

--- Codes/Android/app/src/main/java/com/example/showtime/LiveChat.java
+++ Codes/Android/app/src/main/java/com/example/showtime/LiveChat.java
@@ -11,4 +11,25 @@ public class LiveChat extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_live_chat);
     }
+    if(FirebaseAuth.getInstance().getCurrentUser() == null) {
+    // Start sign in/sign up activity
+    startActivityForResult(
+            AuthUI.getInstance()
+            .createSignInIntentBuilder()
+            .build(),
+            SIGN_IN_REQUEST_CODE
+    );
+} else {
+    // User is already signed in. Therefore, display
+    // a welcome Toast
+    Toast.makeText(this,
+            "Welcome " + FirebaseAuth.getInstance()
+                    .getCurrentUser()
+                    .getDisplayName(),
+            Toast.LENGTH_LONG)
+            .show();
+ 
+    // Load chat room contents
+    displayChatMessages();
+}
 }
