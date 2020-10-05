da8fe59d5b3ab0b457555d9bf80a71967c39af73
pranay0987 <pranaykumar0987@gmail.com>
2020-10-04 21:00:50 -0500

    reading the data into the application

--- Codes/Android/app/src/main/java/com/example/showtime/ViewTicket.java
+++ Codes/Android/app/src/main/java/com/example/showtime/ViewTicket.java
@@ -11,4 +11,23 @@ public class ViewTicket extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_ticket);
     }
+
+    DocumentReference docRef = db.collection("movies").document("amal");
+
+    // Source can be CACHE, SERVER, or DEFAULT.
+    Source source = Source.CACHE;
+
+// Get the document, forcing the SDK to use the offline cache
+docRef.get(source).addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+        @Override
+        public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+            if (task.isSuccessful()) {
+                // Document found in the offline cache
+                DocumentSnapshot document = task.getResult();
+                Log.d(TAG, "Cached document data: " + document.getData());
+            } else {
+                Log.d(TAG, "Cached get failed: ", task.getException());
+            }
+        }
+    });
 }
