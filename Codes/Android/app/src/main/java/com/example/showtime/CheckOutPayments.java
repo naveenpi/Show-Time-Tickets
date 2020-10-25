456c52e811eade486a73db7cb91774a127c37da5
pranay0987 <pranaykumar0987@gmail.com>
2020-10-25 17:33:58 -0500

    added code for the recycler view in CheckOutPayments.java

--- Codes/Android/app/src/main/java/com/example/showtime/CheckOutPayments.java
+++ Codes/Android/app/src/main/java/com/example/showtime/CheckOutPayments.java
@@ -31,3 +31,57 @@ public class CheckOutPayments extends AppCompatActivity {
         recyclerView = findViewById(R.id.recyclerViewListPaymentsCheckOut);
         constructRecyclerView();
     }
+
+    private void constructRecyclerView() {
+
+        Query query = firebaseFirestore.collection("payments").whereEqualTo("user",mAuth.getCurrentUser().getUid());
+        Log.d("payment query",query.toString());
+        FirestoreRecyclerOptions<PaymentsModel> response = new FirestoreRecyclerOptions.Builder<PaymentsModel>()
+                .setQuery(query, PaymentsModel.class)
+                .build();
+
+        adapter = new PaymentsAdapter(response);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(adapter);
+
+        new ItemTouchHelper(new ItemTouchHelper.SimpleCallback(0,ItemTouchHelper.LEFT |ItemTouchHelper.RIGHT) {
+
+            @Override
+            public boolean onMove(@NonNull RecyclerView recyclerView, @NonNull RecyclerView.ViewHolder viewHolder, @NonNull RecyclerView.ViewHolder target) {
+                return false;
+            }
+
+            @Override
+            public void onSwiped(@NonNull RecyclerView.ViewHolder viewHolder, int direction) {
+                adapter.deleteItem(viewHolder.getAdapterPosition());
+            }
+
+        }).attachToRecyclerView(recyclerView);
+
+        adapter.setOnItemClickListener(new PaymentsAdapter.OnItemClickListener() {
+            @Override
+            public void clickOnItem(DocumentSnapshot documentSnapshot, int position) {
+
+                Intent i = getIntent();
+                Intent toTicket = new Intent(getApplicationContext(), ViewTicket.class);
+                toTicket.putExtra("id",i.getStringExtra("id"));
+                toTicket.putExtra("details",i.getStringExtra("details"));
+                toTicket.putExtra("img",i.getStringExtra("img"));
+                startActivity(toTicket);
+            }
+        });
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        adapter.stopListening();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        adapter.startListening();
+    }
+}
