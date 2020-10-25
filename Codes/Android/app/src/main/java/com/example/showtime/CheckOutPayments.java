78caae2bf66a6d42ed5f34f663ef5c2562004967
praneeth323 <46974375+praneeth323@users.noreply.github.com>
2020-10-25 17:25:45 -0500

    Added the CheckOutPayments.java and activity_check_out_payments.xml

---
+++ Codes/Android/app/src/main/java/com/example/showtime/CheckOutPayments.java
@@ -0,0 +1,33 @@
+package com.example.showtime;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.ItemTouchHelper;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.firebase.ui.firestore.FirestoreRecyclerOptions;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
+
+public class CheckOutPayments extends AppCompatActivity {
+
+
+    RecyclerView recyclerView;
+    FirebaseFirestore firebaseFirestore = FirebaseFirestore.getInstance();
+    PaymentsAdapter adapter;
+    FirebaseAuth mAuth = FirebaseAuth.getInstance();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_check_out_payments);
+        recyclerView = findViewById(R.id.recyclerViewListPaymentsCheckOut);
+        constructRecyclerView();
+    }
